580c9006c10256c7930ccb6529deb927bf0185d2
johnCabahug <Ninzcabahug701@gmail.com>
2026-05-21 17:22:00 +0800

    fix: use path.join for swagger.yaml to work after TypeScript compile

--- node-mysql-api/_helpers/swagger.ts
+++ node-mysql-api/_helpers/swagger.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import path from "path";
 const router = express.Router();
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
-const swaggerDocument = YAML.load("./swagger.yaml");
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 export default router;
