ce4e5c8f9648d219a2f03f40df0b80f8a5ac5358
johnCabahug <Ninzcabahug701@gmail.com>
2026-05-21 17:31:52 +0800

    fix: pass port to Sequelize connection

--- node-mysql-api/_helpers/db.ts
+++ node-mysql-api/_helpers/db.ts
@@ -28,6 +28,7 @@ async function initialize() {
   const sequelize = new Sequelize(database, user, password, {
     dialect: "mysql",
     host: host,
+    port: port,
   });
 
   db.Account = accountModel(sequelize);
